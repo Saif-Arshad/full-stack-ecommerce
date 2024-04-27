c7af6a58e11a14a20d6aa685b4c872ef89280d97
Saif Ur Rehman <saifurrehman.web@gmail.com>
2024-04-27 16:54:12 +0500

    making new arriving section

---
+++ src/Data/Products.js
@@ -0,0 +1,36 @@
+import poloshirt from '../../public/Assets/Images/Stoke/black polo shirt.jpg'
+import blueshirt from '../../public/Assets/Images/Stoke/blue plain shirt.jpg'
+import blueswaetshirt from '../../public/Assets/Images/Stoke/blue sweatshirt.jpg'
+import darkblue from '../../public/Assets/Images/Stoke/dark blue shirt.jpg'
+import denimjacket from '../../public/Assets/Images/Stoke/denim jacket.jpg'
+import outcast from '../../public/Assets/Images/Stoke/outcast t shirt.jpg'
+import plainwhite from '../../public/Assets/Images/Stoke/plain white shirt.jpg'
+import poloplain from '../../public/Assets/Images/Stoke/polo plain shirt.jpg'
+
+
+export const Produts = [
+    {
+            "title" : "plain white shirt",
+            "image": plainwhite,
+            "discountprice": "29.00",
+            "actualprice": "59.00",
+            category:[ "men,t shirt , plain shirt"],
+            Tags: ["Modern,Design, Cotton"],
+            "description": `
+            Elevate your style with our timeless Plain White Shirt. Crafted from high-quality cotton, it offers comfort and versatility for any occasion. Tailored fit, classic design, and durable stitching make it a wardrobe essential.`,
+
+        },
+    {
+            "title" : "Denim jacket",
+            "image": denimjacket,
+            "discountprice": "69.00",
+            "actualprice": "89.00",
+            category:[ "unisex,jacket , denim"],
+            Tags: ["Modern,Design, Cotton"],
+            "description": `
+            
+Elevate your style with our timeless Plain White Shirt. Crafted from high-quality cotton, it offers comfort and versatility for any occasion. Tailored fit, classic design, and durable stitching make it a wardrobe essential.`,
+
+        },
+    ]
+
