405d2f874cf5e8624f02c7e5982f3a245e144ffd
Saif Ur Rehman <132079173+Saif-Arshad@users.noreply.github.com>
2024-05-01 17:08:16 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Providers } from "@/Redux/Provider";
 import Footer from "@/Components/Footer/Footer";
 import { Toaster } from "react-hot-toast";
 export const metadata = {
-  title: "Ecommerce",
+  title: "NorthStar - Buy for your love ones ",
 };
 
 export default function RootLayout({ children }) {
